a76d6f521d71a9f6b4eeb8015f234fa017cb1bde
11-Julian <11julian99@gmail.com>
2024-03-16 21:36:31 -0500

    Initial Commit

---
+++ 2Tabs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2Tabs
+//
+//  Created by Julian on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2Tabs/_TabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _TabsApp.swift
+//  2Tabs
+//
+//  Created by Julian on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct _TabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
